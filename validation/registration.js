476f09e029c299dd6ca1269d0a9cafc67bed08e0
Fed-up <tomcurphey12@gmail.com>
2018-06-10 23:44:41 +1000

    Started Redux

--- validation/registration.js
+++ validation/registration.js
@@ -17,12 +17,12 @@ module.exports = function validateRegisterInput(data) {
     errors.name = 'Name field is required';
   }
 
-  if (Validator.isEmpty(data.email)) {
-    errors.email = 'Email field is required';
-  }
-
   if (!Validator.isEmail(data.email)) {
     errors.email = 'Email is an invalid email';
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required';
   }
 
   if (Validator.isEmpty(data.password)) {
